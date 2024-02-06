0f5e7c1c47a108e4d65548bc47d59a298a6fc6ee
Daan Schouten <69320678+dnschouten@users.noreply.github.com>
2024-02-06 12:02:27 +0100

    first start on loftr keypoint matcher

--- src/keypoints.py
+++ src/keypoints.py
@@ -1,8 +1,9 @@
 import numpy as np
 import torch
 import cv2
+import kornia as K
 from typing import List, Any
-from lightglue import LightGlue, SuperPoint
+from lightglue import LightGlue, SuperPoint, DISK
 from lightglue.utils import rbd
 from modules.models.DALF import DALF_extractor as DALF
 
@@ -28,16 +29,16 @@ def get_lightglue_keypoints(ref_image: np.ndarray, moving_image: np.ndarray) -> 
     """
 
     # Initialize lightglue detector and matcher
-    lightglue_detector = SuperPoint(max_num_keypoints=8192).eval().cuda()  
+    lightglue_detector = SuperPoint(max_num_keypoints=None).eval().cuda()  
     lightglue_matcher = LightGlue(features='superpoint').eval().cuda() 
 
     # Convert images to tensor
-    ref_image_tensor = torch.tensor(ref_image.transpose((2, 0, 1)) / 255., dtype=torch.float).cuda()
-    moving_image_tensor = torch.tensor(moving_image.transpose((2, 0, 1)) / 255., dtype=torch.float).cuda()
+    ref_tensor = torch.tensor(ref_image.transpose((2, 0, 1)) / 255., dtype=torch.float).cuda()
+    moving_tensor = torch.tensor(moving_image.transpose((2, 0, 1)) / 255., dtype=torch.float).cuda()
 
     # Extract features
-    ref_features = lightglue_detector.extract(ref_image_tensor)
-    moving_features = lightglue_detector.extract(moving_image_tensor)
+    ref_features = lightglue_detector.extract(ref_tensor)
+    moving_features = lightglue_detector.extract(moving_tensor)
 
     # Find matches with features from reference image
     matches01 = lightglue_matcher({'image0': ref_features, 'image1': moving_features})
@@ -49,6 +50,35 @@ def get_lightglue_keypoints(ref_image: np.ndarray, moving_image: np.ndarray) -> 
     points_moving = np.float32([i.astype("int") for i in moving_features['keypoints'][matches[..., 1]].cpu().numpy()])
 
     return points_ref, points_moving
+
+
+def get_loftr_keypoints(ref_image: np.ndarray, moving_image: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+    First try with the LoFTR keypoint detector and matcher.
+    """
+
+    # Convert to grayscale
+    ref_image = cv2.cvtColor(ref_image, cv2.COLOR_RGB2GRAY)
+    moving_image = cv2.cvtColor(moving_image, cv2.COLOR_RGB2GRAY)
+
+    # Convert to tensor with Kornia
+    ref_tensor = K.image_to_tensor(ref_image, None).float() / 255.0
+    moving_tensor = K.image_to_tensor(moving_image, None).float() / 255.0
+
+    # Rescale to fit LoFTR architecture
+    ref_tensor = K.resize(ref_tensor, (480, 640), antialias=True).cuda()
+    moving_tensor = K.resize(moving_tensor, (480, 640), antialias=True).cuda()
+
+    # Get matcher and match
+    matcher = K.feature.LoFTR(pretrained="indoor_new").eval().cuda()
+    input = {"image0": ref_tensor, "image1": moving_tensor}
+    with torch.no_grad():
+        matches = matcher(input)
+
+    ref_points = matches["keypoints0"].cpu().numpy()
+    moving_points = matches["keypoints1"].cpu().numpy()
+
+    return ref_points, moving_points
 
 
 def get_dalf_keypoints(ref_image: np.ndarray, moving_image: np.ndarray, device: Any) -> tuple[np.ndarray, np.ndarray]:
